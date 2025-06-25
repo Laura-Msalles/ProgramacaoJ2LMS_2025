526ac7a19fb73d844b60a9189ba38f79a39b8add
Laura-Msalles <163336145+Laura-Msalles@users.noreply.github.com>
2025-06-25 01:51:19 -0300

    inserida a função para mudar a cor do nome-usuário

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function inserirNome () {
     let nomeUsuario = prompt("Qual o seu Nome");
     let elemento = document.querySelector("nome-usuário");
     elemento.textContent = nomeUsuario;
-}
-inserirNome();
+
+function mudarCor(){
+    const elemento = document.getElementById("nome-usuário"); 
+    elemento.style.color = "red"; 
+} 
+mudarCor();
