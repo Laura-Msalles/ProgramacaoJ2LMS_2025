f6cdbb512f9b978dfb981c2d4db7e9a254b54cc6
Laura-Msalles <163336145+Laura-Msalles@users.noreply.github.com>
2025-07-29 18:46:03 -0300

    alteração do script

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function mudarCor(){
     elemento.style.color = "pink";
    } 
  mudarCor();
+let linguagens = ["JavaScript","Python","C"];​
+console.log(linguagens[0]);
+
+const item = document.querySelector("#lista");
+item.textcontent =  `${linguagens[0]}, ${linguagens[1]} e ${linguagens[2]}`;
+
+let aluno = {
+    nome: "Gabriel",
+    idade: 13,
+    anoLetivo: "2ª série",
+    materiasFavoritas: ["Geografia", "Programação e Artes"]
+}
